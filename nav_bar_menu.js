a0728f80d557426323b8efce5939c373c753eff2
Augustusrossi <147848584+Augustusrossi@users.noreply.github.com>
2024-07-15 21:57:04 -0300

    Add files via upload

---
+++ nav_bar_menu.js
@@ -0,0 +1,9 @@
+function abrirNav() {
+    document.getElementById("menu_left_oculto").style.width="300px";
+    document.getElementById("principal").style.marginLeft="300px";
+}
+
+function fecharNav() {
+    document.getElementById("menu_left_oculto").style.width="0";
+    document.getElementById("principal").style.marginLeft="0";   
+}
